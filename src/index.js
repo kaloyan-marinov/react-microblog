e72f04d20705752dc976b6fc5cb100cc2804eb3f
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-05-27 16:32:19 +0300

    clean up `src/index.js`

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,6 +12,12 @@ root.render(
   </React.StrictMode>
 );
 
+/*
+You are not going to use this in this project,
+but in case you are interested,
+generating Web Vitals metrics is an optional feature of React
+that allows you to analyze the performance of your application.
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
