e13dd1afae7db9edde1de3b3dcd508d760c94bd5
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-05-27 20:42:27 +0300

    create a `<Header>` and utilize it

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 import Container from "react-bootstrap/Container";
+import Header from "./components/Header";
 
 export default function App() {
   const posts = [
     {
       id: 1,
-      text: 'Hello, world!',
-      timestamp: 'a minute ago',
+      text: "Hello, world!",
+      timestamp: "a minute ago",
       author: {
-        username: 'susan',
+        username: "susan",
       },
     },
     {
       id: 2,
-      text: 'Second post',
-      timestamp: 'an hour ago',
+      text: "Second post",
+      timestamp: "an hour ago",
       author: {
-        username: 'john',
+        username: "john",
       },
     },
   ];
 
   return (
     <Container fluid className="App">
-      <h1>Microblog</h1>
-      {
-        posts.length === 0 ?
+      <Header />
+      <Container>
+        {posts.length === 0 ? (
           <p>There are no blog posts.</p>
-      :
-        posts.map(post => {
-          return (
-            <p key={post.id}>
-              <b>{post.author.username}</b> &mdash; {post.timestamp}
-              <br />
-              {post.text}
-            </p>
-          );
-        })
-      }
+        ) : (
+          posts.map((post) => {
+            return (
+              <p key={post.id}>
+                <b>{post.author.username}</b> &mdash; {post.timestamp}
+                <br />
+                {post.text}
+              </p>
+            );
+          })
+        )}
+      </Container>
     </Container>
   );
 }
