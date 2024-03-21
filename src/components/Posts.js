f99b040c4ef6067fbb92d4e04125741fbe966b24
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-03-21 16:47:38 +0100

    arrange for the frontend's `<Posts>` to issue an HTTP request to the backend (for retrieving "remote(ly-stored) data" about posts) by creating a side effect function, which is configured to run once - when[/after] the component is first rendered - and never again; for the sake of clarity, "a side effect" is React jargon for "a background operation"

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,23 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Spinner from "react-bootstrap/Spinner";
+
+const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 
 export default function Posts() {
   const [posts, setPosts] = useState();
+
+  /*
+  React requires that
+  the function that is given as the argument to `useEffect()`
+  should not be async.
+  
+  A commonly used trick
+  to enable the use of `async` and `await` in side effect functions is
+  to create an inner async function and immediately call it.
+  This pattern is commonly referred as
+  an Immediately Invoked Function Expression (IIFE).
+  */
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(BASE_API_URL + "/api/feed");
+
+      if (response.ok) {
+        const results = await response.json();
+        setPosts(results.data);
+      } else {
+        // To make this component robust,
+        // it is also necessary to handle the case of the request failing.
+        setPosts(null);
+      }
+    })();
+  }, []);
 
   return (
     <>
       {posts === undefined ? (
         <Spinner animation="border" />
       ) : (
-        posts.map((post) => {
-          return (
-            <p key={post.id}>
-              <b>{post.author.username}</b> &mdash; {post.timestamp}
-              <br />
-              {post.text}
-            </p>
-          );
-        })
+        <>
+          {posts === null ? (
+            <p>Could not retrieve blog posts.</p>
+          ) : (
+            <>
+              {posts.map((post) => {
+                return (
+                  <p key={post.id}>
+                    <b>{post.author.username}</b> &mdash; {post.timestamp}
+                    <br />
+                    {post.text}
+                  </p>
+                );
+              })}
+            </>
+          )}
+        </>
       )}
     </>
   );
