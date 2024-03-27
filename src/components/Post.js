a8c365458294346cb6c9e1fe606183c5cac88af2
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-03-27 06:01:29 +0100

    memoize the component exported by `src/components/Post.js`

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,35 @@
+import { memo } from "react";
 import Stack from "react-bootstrap/Stack";
 import Image from "react-bootstrap/Image";
 import { Link } from "react-router-dom";
 import TimeAgo from "./TimeAgo";
 
-export default function Post({ post }) {
+// Adding the `memo` function from React as a wrapper
+// transparently replaces the original component
+// with a version of it
+// that is optimized to skip the render
+// when it is invoked with the same inputs [= props and state variables] as the previous time.
+//
+// Q:
+//    Should all components be memoized?
+// A:
+//    This is a difficult question to answer.
+//
+//    It is easy to visualize the performance improvements for components such as `Post`,
+//    which are often re-rendered by React, just because the parent renders.
+//
+//    It is hard to say that
+//    it is beneficial to memoize components
+//    that do not render as often,
+//    or that generally render with different inputs,
+//    because the logic added by `memo()` also has a cost.
+//
+//    If you are interested in finding out
+//    if applying `memo()` to a component makes your application render faster,
+//    a specialized tool such as the profiler included in
+//    the React Developer Tools plugin for Chrome or Firefox
+//    should be used to take accurate measurements.
+export default memo(function Post({ post }) {
   return (
     <Stack direction="horizontal" gap={3} className="Post">
       <Image
@@ -23,4 +49,4 @@ export default function Post({ post }) {
       </div>
     </Stack>
   );
-}
+});
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,6 +5,35 @@ import Post from "./Post";
 import More from "./More";
 import Write from "./Write";
 
+// In React, renders are carried out in two phases.
+// First, the output of the render functions is applied to _a virtual DOM_,
+// which is an internal copy of the actual DOM from the browser.
+// Once the components are rendered on the virtual DOM,
+// the second phase invokes an efficient algorithm
+// to merge the virtual DOM to the real DOM,
+// and this is what makes the components visible on the page.
+//
+// Sometimes a component renders
+// even though none of its inputs or state variables have changed.
+//
+// Unfortunately unnecessary renders are more common than most developers think.
+// When a component renders, it returns a new JSX subtree.
+// Any child components that are included in the returned JSX
+// are forced to re-render
+// even if they don't change,
+// simply because they generated anew during the parent's render.
+//
+// As a general rule, you can say:
+//    Unless measures are taken to prevent it,
+//    each time a component renders,
+//    any child components included in the JSX returned have to render too.
+//
+// In many cases the cost of these unnecessary renders is negligible,
+// but this isn't always the case.
+// Consider the following component,
+// which has X instances of the `Post` component.
+// If the user clicks the "More" button to obtain an additional `X` posts,
+// all `X + X` posts will re-render.
 export default function Posts({ content, write }) {
   const [posts, setPosts] = useState();
   const [pagination, setPagination] = useState();
