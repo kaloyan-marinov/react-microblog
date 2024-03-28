69da0941a2d1c9f8a6e701938b1c275cfdae1a7a
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-03-28 11:57:12 +0100

    enhance the test for `<FlashMessage>`, which was added by the preceding commit, by ensuring that the alert will _close_ - not _be removed_ ! - after X seconds, without having to actually wait those X seconds

--- src/components/FlashMessage.js
+++ src/components/FlashMessage.js
@@ -6,6 +6,22 @@ import { FlashContext } from "../contexts/FlashProvider";
 export default function FlashMessage() {
   const { flashMessage, visible, hideFlash } = useContext(FlashContext);
 
+  // Since what this component renders to the DOM is wrapped within a `<Collapse>`,
+  // writing an test for whether the "`<Alert>`" closes automatically after X seconds
+  // is complicated by that top-level `<Collapse>`.
+  // The complication resides in
+  // the following implementation detail related to `<Collapse>`:
+  // it (and therefore its contents) aren't ever removed or hidden;
+  // instead, it uses CSS transitions to shrink the "alert"
+  // until it reaches a height of 0 pixels (instead of actually hiding it).
+  //
+  // One way to facilitate writing the above-mentioned test
+  // relies on having knowledge of the inner workings of the `Collapse` component,
+  // which is suboptimal
+  // because the test could break if React-Bootstrap is upgraded to a new version.
+  //
+  // A surprisingly simple solution is to add a custom data attribute to the `<Alert>`
+  // indicating whether it is visible or not.
   return (
     <Collapse in={visible}>
       <div>
@@ -13,6 +29,7 @@ export default function FlashMessage() {
           variant={flashMessage.type || "info"}
           dismissible
           onClose={hideFlash}
+          data-visible={visible}
         >
           {flashMessage.message}
         </Alert>
--- src/contexts/FlashProvider.test.js
+++ src/contexts/FlashProvider.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import { useEffect } from "react";
 import FlashProvider from "./FlashProvider";
 import { useFlash } from "./FlashProvider";
@@ -53,4 +53,43 @@ test("flashes a message", async () => {
     "[mocked] this alert contains critical information!"
   );
   expect(alert).toHaveClass("alert-danger");
+  expect(alert).toHaveAttribute("data-visible", "true");
+
+  // The `act()` function, which is provided by the React Testing Library, will
+  // call the function passed as an argument
+  // (
+  // which might change some state variables that will require some re-renders
+  // and that in turn might launch new side effect functions that might require even more re-renders
+  // )
+  // and then wait for the React application to settle down.
+  //
+  // In essence, the `act()` function is designed
+  // to wait for such asynchronous activity to complete
+  // (i.e. for all state variables, side effects, and re-renders to settle).
+  //
+  // It is important to emphasize that
+  // calling `jest.runAllTimers()` directly does not perform the above-mentioned "safety wait".
+  // To wit:
+  //    if the next statement is replaced with a direct call to `jest.runAllTimers()`,
+  //    running this test in a terminal session would FAIL
+  //    and the following would be displayed in the terminal session:
+  //    ```
+  //    console.error
+  //    Warning: An update to Transition inside a test was not wrapped in act(...).
+  //
+  //    When testing, code that causes React state updates should be wrapped into act(...):
+  //
+  //    # ...
+  //
+  //    console.error
+  //    Warning: An update to FlashProvider inside a test was not wrapped in act(...).
+  //    ```
+
+  // When testing, code that causes React state updates should be wrapped into act(...):
+  act(() => {
+    // Advance [Jest's simulation or hijacked-version or mocked substitute of] the time
+    // until (the moment when) the next timer fires.
+    jest.runAllTimers();
+  });
+  expect(alert).toHaveAttribute("data-visible", "false");
 });
